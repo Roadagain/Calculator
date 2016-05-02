3370015957c0ea6d0bb4e0484917462ae89cec6e
Roadagain <ringoh72@gmail.com>
2016-05-02 10:34:01 +0900

    Loop until entered exit or quit command

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char input[65536];
+    while (1){
+        char input[65536];
 
-    fgets(input, 65535, stdin);
+        fgets(input, 65535, stdin);
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0){
+            break;
+        }
+    }
 
     return (0);
 }

cdad2efa340aa999d87fe183e6e56f5ffd68c9bf
Roadagain <ringoh72@gmail.com>
2016-07-07 12:58:23 +0900

    Print only integer part if answer is integer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #include "calc.h"
 #include "input.h"
@@ -8,12 +9,19 @@ int main(void)
 {
     while (1){
         char input[65536];
+        double ans;
 
         read_line(input);
         if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0){
             break;
         }
-        printf("%f\n", calc(input, 0));
+        ans = calc(input, 0);
+        if (floor(ans) == ans){
+            printf("%.0f\n", ans);
+        }
+        else{
+            printf("%f\n", calc(input, 0));
+        }
     }
 
     return (0);

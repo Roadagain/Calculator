9b8146e89f93f8a3fdc28d8391ee6afb5895125c
Roadagain <ringoh72@gmail.com>
2017-05-25 21:50:07 +0900

    Add tests for input

---
+++ test/replace_minus.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "../input.h"
+
+int main(void)
+{
+    char before[] = "-2+1";
+    char after[] = "(0-2)+1";
+
+    replace_minus(before);
+    if (strcmp(before, after) == 0){
+        return (0);
+    }
+    else {
+        return (-1);
+    }
+}

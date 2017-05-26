6d3cc545a4734a184802d583cf7f95561bf26eb5
Roadagain <ringoh72@gmail.com>
2017-05-26 18:50:31 +0900

    Print before and after on replace_minus test

--- test/input/replace_minus.c
+++ test/input/replace_minus.c
@@ -6,7 +6,9 @@ int main(void)
     // a case removable
     char before1[64] = "-2+1";
     char after1[64] = "(0-2)+1";
+    puts(before1);
     replace_minus(before1);
+    puts(before1);
     if (strcmp(before1, after1) != 0){
         return (-1);
     }
@@ -14,7 +16,9 @@ int main(void)
     // a case not removable
     char before2[64] = "3-2";
     char after2[64] = "3-2";
+    puts(before2);
     replace_minus(before2);
+    puts(before2);
     if (strcmp(before2, after2) != 0){
         return (-1);
     }
